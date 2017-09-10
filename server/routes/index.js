cc9ce9e926255660de724c2e50d4a8fd48d07604
kai <yangkai@szzbmy.com>
2017-09-10 11:16:50 +0800

    add babel es6

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+// const path = require('path');
+// const controllerPath = path.join(__dirname, '../controller');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// const index = require(`${controllerPath}/index.js`);
+// const login = require(`${controllerPath}/login.js`);
+// const user = require(`${controllerPath}/user.js`);
 
-module.exports = router;
+// const routes = (app) => {
+//   app.use('/', index);
+//   app.use('/login', login);
+//   app.use('/user', user);
+// };
+
+// module.exports = routes
+// 
+import index from '../controller/index.js';
+import login from '../controller/login.js';
+import user from '../controller/user.js';
+
+const routes = (app) => {
+  app.use('/', index);
+  app.use('/login', login);
+  app.use('/user', user);
+};
+
+export default routes
---
+++ server/server.js
@@ -0,0 +1,51 @@
+// const path = require('path');
+import path from 'path';
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const express = require('express');
+// const app = require('./app.js');
+const app = express();
+// const routes = require('./routes');
+import routes from './routes';
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 设置路由
+routes(app);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const server = app.listen(3000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Example app listening at http://%s:%s', 'localhost', port);
+});
