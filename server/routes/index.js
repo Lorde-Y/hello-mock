44189860a5e7257fee7de9c8b48dbe5be0746f2e
kai <yangkai@szzbmy.com>
2017-09-10 14:07:10 +0800

    add server-node for eslint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,3 @@
-// const path = require('path');
-// const controllerPath = path.join(__dirname, '../controller');
-
-// const index = require(`${controllerPath}/index.js`);
-// const login = require(`${controllerPath}/login.js`);
-// const user = require(`${controllerPath}/user.js`);
-
-// const routes = (app) => {
-//   app.use('/', index);
-//   app.use('/login', login);
-//   app.use('/user', user);
-// };
-
-// module.exports = routes
-// 
 import index from '../controller/index.js';
 import login from '../controller/login.js';
 import user from '../controller/user.js';
@@ -23,4 +8,4 @@ const routes = (app) => {
   app.use('/user', user);
 };
 
-export default routes
+export default routes;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
-// const path = require('path');
 import path from 'path';
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+import routes from './routes';
+// const favicon = require('serve-favicon');
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import express from 'express';
 
-const express = require('express');
 // const app = require('./app.js');
 const app = express();
-// const routes = require('./routes');
-import routes from './routes';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,14 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 routes(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -45,7 +43,7 @@ app.use(function(err, req, res, next) {
 });
 
 const server = app.listen(3000, function () {
-  const host = server.address().address;
+  // const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', 'localhost', port);
 });
